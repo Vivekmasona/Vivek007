7ffdf7d4a0ada7204ef2b1f66e28f110d280e6e1
Doggie <79816938+Chris22177@users.noreply.github.com>
2023-04-29 18:53:17 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const ytdl = require('ytdl-core');
 const cors = require('cors');
-const path = require('path');
+const { join } = require('path');
 const app = express();
 
 app.use(cors());
 
-app.use("/css", express.static(path.join(__dirname, "src", "css")));
-app.use("/js", express.static(path.join(__dirname, "src", "js")));
+app.use('/images', express.static(join(__dirname, 'src', 'css')));
+app.use('/css', express.static(join(__dirname, 'src', 'css')));
+app.use('/js', express.static(join(__dirname, 'src', 'js')));
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -15,18 +16,18 @@ app.use(function (req, res, next) {
 });
 
 app.get('/', (req,res) => {
-  res.sendFile(path.join(__dirname, "src", "html", "Convert.html"));
+  res.sendFile(join(__dirname, 'src', 'html', 'Convert.html'));
 });
 
 app.get('/view', async (req,res) => {  
 var URL = req.query.url;
   
-if(!URL || !ytdl.validateURL(URL)) return res.sendFile(path.join(__dirname, "src", "html", "NotFound.html"));
+if(!URL || !ytdl.validateURL(URL)) return res.sendFile(join(__dirname, 'src', 'html', 'NotFound.html'));
 var Video = await ytdl.getBasicInfo(URL);
 
-res.header("Content-Disposition", `inline; filename="${Video.videoDetails.title}.mp4"`);
-res.header("Content-Type", "video/mp4");
-res.header("Content-Transfer-Encoding", "binary");
+res.header('Content-Disposition', `inline; filename='${Video.videoDetails.title}.mp4'`);
+res.header('Content-Type', 'video/mp4');
+res.header('Content-Transfer-Encoding', 'binary');
  
 const info = await ytdl.getInfo(ytdl.getURLVideoID(URL));
 const format = ytdl.chooseFormat(info.formats, { quality: 22 });
@@ -37,7 +38,7 @@ ytdl(URL, {
 });
 
 app.use(function(req, res, next) {
-  res.status(404).sendFile(path.join(__dirname, "src", "html", "404.html"));
+  res.status(404).sendFile(join(__dirname, 'src', 'html', '404.html'));
 });
 
 app.listen(8080, () => {
