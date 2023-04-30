3f0e2ba1f98f7dee4c98848722f3819701d938d3
Doggie <79816938+Chris22177@users.noreply.github.com>
2023-04-30 15:02:12 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const ytdl = require('ytdl-core');
 const cors = require('cors');
-const { join } = require('path');
+const path = require('path');
 const app = express();
+
+const fs = require('node:fs');
+const { spawn } = require('child_process');
 
 app.use(cors());
 
-app.use('/images', express.static(join(__dirname, 'src', 'css')));
-app.use('/css', express.static(join(__dirname, 'src', 'css')));
-app.use('/js', express.static(join(__dirname, 'src', 'js')));
+app.use("/css", express.static(path.join(__dirname, "src", "css")));
+app.use("/js", express.static(path.join(__dirname, "src", "js")));
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -16,31 +18,58 @@ app.use(function (req, res, next) {
 });
 
 app.get('/', (req,res) => {
-  res.sendFile(join(__dirname, 'src', 'html', 'Convert.html'));
+  res.sendFile(path.join(__dirname, "src", "html", "Convert.html"));
 });
 
-app.get('/view', async (req,res) => {  
-var URL = req.query.url;
-  
-if(!URL || !ytdl.validateURL(URL)) return res.sendFile(join(__dirname, 'src', 'html', 'NotFound.html'));
-var Video = await ytdl.getBasicInfo(URL);
+app.get('/view', async (req, res) => {
+  var url = req.query.url;
 
-res.header('Content-Disposition', `inline; filename='${Video.videoDetails.title}.mp4'`);
-res.header('Content-Type', 'video/mp4');
-res.header('Content-Transfer-Encoding', 'binary');
- 
-const info = await ytdl.getInfo(ytdl.getURLVideoID(URL));
-const format = ytdl.chooseFormat(info.formats, { quality: 22 });
+  if (!url || !ytdl.validateURL(url)) {
+    return res.sendFile(path.join(__dirname, "src", "html", "NotFound.html"));
+  }
 
-ytdl(URL, {
-    format: format => format
-    }).pipe(res); 
+  var Video = await ytdl.getBasicInfo(url);
+
+  const info = await ytdl.getInfo(ytdl.getURLVideoID(url));
+  const videoFormat = ytdl.chooseFormat(info.formats, { quality: 'highestvideo', format: 'mp4' });
+  const audioFormat = ytdl.chooseFormat(info.formats, { filter: 'audioonly' });
+
+  // Download best video and audio
+  const video = ytdl(url, { format: videoFormat });
+  const audio = ytdl(url, { format: audioFormat });
+
+  const ffmpeg = spawn('ffmpeg', [
+    '-i', 'pipe:0',
+    '-i', 'pipe:1',
+    '-c', 'copy',
+    '-f', 'matroska',
+    'pipe:2'
+  ]);
+
+  const headers = {
+    'Content-Type': 'video/mp4',
+    'Content-Disposition': `inline; filename="${Video.videoDetails.title}.mp4"`,
+    'Content-Transfer-Encoding': 'binary'
+  };
+
+  res.writeHead(200, headers);
+
+  video.pipe(ffmpeg.stdin);
+  audio.pipe(ffmpeg.stdin);
+
+  ffmpeg.stdout.pipe(res);
+
+  ffmpeg.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+  });
+
+  ffmpeg.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
 });
 
 app.use(function(req, res, next) {
-  res.status(404).sendFile(join(__dirname, 'src', 'html', '404.html'));
+  res.status(404).sendFile(path.join(__dirname, "src", "html", "404.html"));
 });
 
-app.listen(8080, () => {
-    console.log('Server Running.');
-});
+app.listen(8080);
