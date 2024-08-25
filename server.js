95fc282119d9f0fae5dd1a8365c0629e6da45d59
Chris <79816938+doggiexb@users.noreply.github.com>
2024-08-25 19:59:48 -0400

    Update server.js

--- server.js
+++ server.js
@@ -88,4 +88,6 @@ app.use(function(req, res, next) {
   res.status(404).sendFile(path.join(__dirname, 'src', 'html', '404.html'));
 });
 
-app.listen(8080);
+app.listen(8080, => () {
+  console.log("Running.");
+});
